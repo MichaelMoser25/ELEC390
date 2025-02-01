fb574fb68fb1642030b753f8c67b40b227a884ae
Br15an <132475888+Br15an@users.noreply.github.com>
2025-02-01 14:11:37 -0500

    Add files via upload

---
+++ test_speaker.py
@@ -0,0 +1,780 @@
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+Muslc = Music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
+from robot_hat import Music
+
+music = music()
+
+def main():
+    flag_bgm = True
+    music.music_set_volume(100)
+
+    while(True):
+        if flag_bgm is True:
+            music.music_play('../musics/slow-trail-Ahjay_Stelino.mps')
+            flag_bgm = False
+
+if __name__ == "__main__":
+    main()
